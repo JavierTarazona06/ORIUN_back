7da3a2ea609cf7859608fe91802908919c37af28
Javier Tarazona J. <developer@engin.ai>
2024-03-08 19:53:55 -0500

    ERD diagram

---
+++ DataBase/creation.sql
@@ -0,0 +1,16 @@
+-- Database: ORIUN
+
+DROP DATABASE IF EXISTS "ORIUN";
+
+CREATE DATABASE "ORIUN"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Colombia.1252'
+    LC_CTYPE = 'Spanish_Colombia.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+	
+	
