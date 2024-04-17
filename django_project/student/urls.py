7beb89c6e9a102cc1818a0def08daa970ebe20f5
Valeria M <vmoras@unal.edu.co>
2024-04-17 08:34:06 -0500

    Fix typo (orthograpy) on url)

--- django_project/student/urls.py
+++ django_project/student/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ApplicationDataView, EligibilityView
 
 urlpatterns = [
-    path('elegible/', EligibilityView.as_view(), name='eligibility'),
+    path('eligible/', EligibilityView.as_view(), name='eligibility'),
     path('info_application/', ApplicationDataView.as_view(), name='application_data'),
 ]
